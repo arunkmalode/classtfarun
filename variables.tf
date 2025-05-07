5067b9cd02ad58c3b41e175789f372a69cd213ec
arunkmalode <arunkmalode@gmail.com>
2025-05-08 00:14:46 +0530

    variables.tf first time

---
+++ variables.tf
@@ -0,0 +1,18 @@
+# Define the resource group for the AKS cluster
+resource "azurerm_resource_group" "aks" {
+  name     = "ArunAKS"
+  location = "West US 2"
+}
+# Define the AKS cluster using kubenet networking
+resource "azurerm_kubernetes_cluster" "aks_cluster" {
+  name                = "arunclsu9090"
+  location            = azurerm_resource_group.aks.location
+  resource_group_name = azurerm_resource_group.aks.name
+  dns_prefix          = "arunclsu9090-dns"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size    = "Standard_DS2_v2"  # Update to a valid VM SKU with >=2 cores and >=4GB RAM
+  }
+}
